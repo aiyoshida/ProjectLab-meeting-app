0e04140572412967c2ea624f4b13d67b1fd730ef
Ai Yoshida <68318468+aiyoshida@users.noreply.github.com>
2025-04-27 01:52:20 +0200

    deleted

--- my-react-app/src/components/NewMeetingCalendar.jsx
+++ my-react-app/src/components/NewMeetingCalendar.jsx
@@ -7,11 +7,6 @@ import interactionPlugin from "@fullcalendar/interaction";
 
 import { useNavigate } from 'react-router-dom';
 
-const CLIENT_ID = '4575472657-7depp48v9d4ct8lhrsf84roap62a2t6g.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyCCyHCUgW89gkolueOCMOwi5-3YU-_yGx8';
-const DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
-const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
-
 export default function NewMeetingCalendar() {
      const navigate = useNavigate();
      const goToHomePage = () => {
