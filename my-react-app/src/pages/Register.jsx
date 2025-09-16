18bdd9110882052affba980cff94fe68799fa700
Ai Yoshida <68318468+aiyoshida@users.noreply.github.com>
2025-09-16 17:47:25 +0900

    google login, envg

--- my-react-app/src/pages/Register.jsx
+++ my-react-app/src/pages/Register.jsx
@@ -1,8 +1,9 @@
 //import './Register.css';
 import icon from '../images/icon.png';
-import { useState } from 'react';
+import { useState, useEffect, useRef  } from 'react';
 import { useNavigate } from 'react-router-dom';
 import moment from "moment-timezone";
+
 
 
 export default function Register() {
@@ -12,6 +13,8 @@ export default function Register() {
   const [gmail, setGmail] = useState('');
 
   const navigate = useNavigate();
+  const divRef = useRef(null);
+  //for holding DOM, value. give this to Google SDK later.
   const goToHomePage = () => {
     navigate('/homepage');
   }
@@ -56,54 +59,36 @@ export default function Register() {
     }
   };
 
+    useEffect(() => {
+    const g = window.google; // obj from Google's SDK
+    if (!g || !divRef.current) return; //if no SDK load, do nothing
+
+    g.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: (resp) => {
+        // ID token in resp.credential
+        fetch("/api/auth/google", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include", // Cookie
+          body: JSON.stringify({ id_token: resp.credential })
+        });
+      },
+    });
+
+    // draw google button to the divRef.current div
+    g.accounts.id.renderButton(divRef.current, {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+
+
 
 
 
   return (
     <div>
-      {/* <div className="register-all">
-        <div className="register-brand-row">
-          <img src={icon} alt="icon" className="register-icon" />
-          <h1 className="register-brand-name">AcrossTime</h1>
-        </div>
-
-        <h2>Create an account</h2>
-        <p>Enter your user name, timezone, and gmail to sign up for this app</p>
-
-        <form onSubmit={handleSubmit}>
-          <input
-            className="register-input"
-            type="text"
-            placeholder="User name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <br /><br />
-          <label className="register-label">
-            <select className="register-input" value={timezone} onChange={(e) => setTimezone(e.target.value)}>
-              <option>Choose your timezone</option>
-              {timezones.map((tz) => (
-                <option key={tz} value={tz}>
-                  {tz}
-                </option>
-              ))}
-
-            </select>
-          </label>
-          <br /><br />
-          <input
-            className="register-input"
-            type="text"
-            placeholder="  email@domain.com"
-            value={gmail}
-            onChange={(e) => setGmail(e.target.value)}
-          />
-          <br /><br />
-          <button className="register-button" type="submit">Create account</button>
-          <br /><br />
-          <button className="register-button" type="button" onClick={handleLogin}> Login</button>
-        </form>
-      </div> */}
       <div className="hero bg-pink-100 min-h-screen">
         <div className="hero-content text-center">
           <div className="max-w-md">
@@ -113,10 +98,12 @@ export default function Register() {
               Coordinate global teams with clarity, speed, and zero confusion.
               Say goodbye to “What time is it for you?” forever.
             </p>
-            <button className="btn bg-white text-black border-[#e5e5e5]" onClick={goToHomePage}> {/*This is temporary navigation*/}
+            {/* <button className="btn bg-white text-black border-[#e5e5e5]" onClick={goToHomePage}> 
               <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
               Login with Google
-            </button>
+            </button> */}
+            <div ref={divRef} /> 
+
           </div>
         </div>
       </div>
