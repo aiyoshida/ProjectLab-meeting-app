7b2bd175f228578cd4ee48ff00bdd85334b76f41
Ai Yoshida <68318468+aiyoshida@users.noreply.github.com>
2025-09-17 23:04:17 +0900

    google login console log

--- my-react-app/src/pages/Register.jsx
+++ my-react-app/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 //import './Register.css';
 import icon from '../images/icon.png';
-import { useState, useEffect, useRef  } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import moment from "moment-timezone";
 
@@ -59,7 +59,7 @@ export default function Register() {
     }
   };
 
-    useEffect(() => {
+  useEffect(() => {
     const g = window.google; // obj from Google's SDK
     if (!g || !divRef.current) return; //if no SDK load, do nothing
 
@@ -73,6 +73,11 @@ export default function Register() {
           credentials: "include", // Cookie
           body: JSON.stringify({ id_token: resp.credential })
         });
+
+        const token = resp.credential;
+        console.log("Raw ID Token:", token);
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        console.log("Decoded payload:", payload);
       },
     });
 
@@ -102,7 +107,7 @@ export default function Register() {
               <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
               Login with Google
             </button> */}
-            <div ref={divRef} /> 
+            <div ref={divRef} />
 
           </div>
         </div>
